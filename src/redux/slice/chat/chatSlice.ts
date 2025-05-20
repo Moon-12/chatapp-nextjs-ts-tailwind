6c6cf56a933286f628cb8c7fb95f7959c61047ae
Moon-12 <ashwijanayak@gmail.com>
2025-05-20 14:38:51 -0400

    thunk for post message removed

--- src/redux/slice/chat/chatSlice.ts
+++ src/redux/slice/chat/chatSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { Timestamp } from "next/dist/server/lib/cache-handlers/types";
 
 export interface Message {
@@ -20,35 +20,6 @@ const initialState: ChatState = {
   error: null,
 };
 
-//can be used with http (if not using socket)
-
-export const sendMessage = createAsyncThunk(
-  "chat/postMessage",
-  async (message: Message, { rejectWithValue }) => {
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/postMessage`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(message),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to send message");
-      }
-
-      const responseData = await response.json();
-      return responseData.data; // Assuming API returns the saved message
-    } catch (error: any) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
-
 export const chatSlice = createSlice({
   name: "chat",
   initialState,
@@ -59,28 +30,6 @@ export const chatSlice = createSlice({
     setNewChat: (state, action) => {
       state.chatData.push(action.payload);
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(sendMessage.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(
-        sendMessage.fulfilled,
-        (state, action: PayloadAction<Message>) => {
-          state.loading = false;
-          if (state.chatData) {
-            state.chatData.push(action.payload);
-          } else {
-            state.chatData = [action.payload];
-          }
-        }
-      )
-      .addCase(sendMessage.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.payload as string;
-      });
   },
 });
 
