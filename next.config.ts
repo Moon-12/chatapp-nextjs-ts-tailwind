772db3d00f39992b7741ab64c6526a3fddbe3a08
Ashwija Nayak <66021345+Moon-12@users.noreply.github.com>
2025-05-25 14:01:57 -0400

    Release prep (#7)
    * deployment prep files added
    
    * type def added for sockjs client
    
    * base path added
    
    * image version changed
    
    * headers added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  basePath: "/chat-app",
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Chat from "../components/chat";
 import DataComponent from "../components/InitialDataLoader";
 import ModalPopup from "../components/modal";
 
 async function fetchChats() {
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/chat/getAllPreviousMessages`,
+      `${process.env.API_BASE_URL}/getAllPreviousMessages`,
       {
         cache: "no-store", // Ensure fresh data
+        headers: {
+          "x-auth-token": "testchatapp",
+        },
       }
     );
     const responseData = await response.json();
