9d988899071c84108ddb40eb50b16a01f20f46ee
Moon-12 <ashwijanayak@gmail.com>
2025-05-28 19:42:13 -0400

    test NEXT_PUBLIC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 import InitialDataLoader from "@/components/InitialDataLoader";
 export default async function Page() {
-  const response = await fetch("/chat-app/api/getBaseURL", {
-    cache: "no-store", // Ensure fresh data for server-side fetching
-  });
-  if (!response.ok) {
-    const text = await response.text(); // Helpful for debugging
-    console.error("Failed API response:", text);
-    throw new Error(`Fetch failed with status ${response.status}`);
-  }
+  // const response = await fetch("/chat-app/api/getBaseURL", {
+  //   cache: "no-store", // Ensure fresh data for server-side fetching
+  // });
+  // if (!response.ok) {
+  //   const text = await response.text(); // Helpful for debugging
+  //   console.error("Failed API response:", text);
+  //   throw new Error(`Fetch failed with status ${response.status}`);
+  // }
 
-  const data = await response.json();
-  const apiBaseUrl = data.baseurl || "undefined";
-  const baseUrl = process.env.API_BASE_URL;
+  // const data = await response.json();
+  // const apiBaseUrl = data.baseurl || "undefined";
+  // const baseUrl = process.env.API_BASE_URL;
   return (
     <>
-      <h1>FRom api {apiBaseUrl}</h1>
+      <h1>FRom api </h1>
 
-      <h1>base url is {baseUrl}</h1>
+      <h1>base url is </h1>
       <h1>base url is {process.env.SERVER_KEY}</h1>
-      <InitialDataLoader url={baseUrl || ""} />
+      <InitialDataLoader url="test" />
     </>
   );
 }
