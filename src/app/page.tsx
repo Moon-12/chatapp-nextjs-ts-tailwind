867fa0ab093738325b10d27525be78bf75c340dc
Moon-12 <ashwijanayak@gmail.com>
2025-05-28 16:17:49 -0400

    use server added at top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+"use server";
 export default async function Page() {
   const baseUrl = process.env.API_BASE_URL;
   return (
     <>
       <h1>base url is {baseUrl}</h1>
+      <h1>base url is {process.env.SERVER_KEY}</h1>
       {/* <DataComponent url={baseUrl || ""} /> */}
     </>
   );
