3e26fc0dc883f7dfe2aeaf47ab0b0687bc003f8e
Moon-12 <ashwijanayak@gmail.com>
2025-05-28 16:02:07 -0400

    test env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import DataComponent from "../components/InitialDataLoader";
-
 export default async function Page() {
   const baseUrl = process.env.API_BASE_URL;
   return (
     <>
-      <h1>base url {baseUrl}</h1>
-      <DataComponent url={baseUrl || ""} />
+      <h1>base url is {baseUrl}</h1>
+      {/* <DataComponent url={baseUrl || ""} /> */}
     </>
   );
 }
