31d05017faa3870ab4375bf403180cba227e1391
Moon-12 <ashwijanayak@gmail.com>
2025-05-28 14:17:06 -0400

    dependency changed

--- src/components/InitialDataLoader.tsx
+++ src/components/InitialDataLoader.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 const InitialDataLoader = ({ url }: { url: string }) => {
   useEffect(() => {
     sessionStorage.setItem("API_BASE_URL", url);
-  }, [url]);
+  }, []);
 
   return null;
 };
